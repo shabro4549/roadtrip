3f6542e3f508c6b1f0ff63646ecd05d8bc43bb48
Shannon Brown <shabro4549@gmail.com>
2020-11-19 19:13:17 -0800

    add whole project

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '9.0'
+
+target 'Roadtrip' do
+  use_frameworks!
+
+  # Pods for Roadtrip
+  pod 'GooglePlaces', '4.0.0'
+  pod 'GoogleMaps', '4.0.0'
+  
+end
--- Roadtrip/ViewController.swift
+++ Roadtrip/ViewController.swift
@@ -6,14 +6,122 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GMSMapViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//
+//    var placesClient: GMSPlacesClient!
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var daysField: UITextField!
+    
+    @IBAction func stepperPressed(_ sender: UIStepper) {
+        daysField.text = String(Int(sender.value))
+    }
+    
+    @IBAction func locationFieldTapped(_ sender: UITextField) {
+        locationField.resignFirstResponder()
+        let acController = GMSAutocompleteViewController()
+        acController.delegate = self
+        present(acController, animated: true, completion: nil)
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+        
 
+        
+//        placesClient = GMSPlacesClient.shared()
+//        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+//        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+//        self.view.addSubview(mapView)
+//
+//                // Creates a marker in the center of the map.
+//                let marker = GMSMarker()
+//                marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+//                marker.title = "Brisbane"
+//                marker.snippet = "Australia"
+//
+//                marker.map = mapView
+        
+//        let fields: GMSPlaceField = GMSPlaceField(rawValue: UInt(GMSPlaceField.name.rawValue) |
+//                                                    UInt(GMSPlaceField.placeID.rawValue))
+//        placesClient?.findPlaceLikelihoodsFromCurrentLocation(withPlaceFields: fields, callback: {
+//          (placeLikelihoodList: Array<GMSPlaceLikelihood>?, error: Error?) in
+//          if let error = error {
+//            print("An error occurred: \(error.localizedDescription)")
+//            return
+//          }
+//
+//          if let placeLikelihoodList = placeLikelihoodList {
+//            for likelihood in placeLikelihoodList {
+//              let place = likelihood.place
+//              print("Current Place name \(String(describing: place.name)) at likelihood \(likelihood.likelihood)")
+//              print("Current PlaceID \(String(describing: place.placeID))")
+//            }
+//          }
+//        })
+    }
+}
+
+//MARK: - CLLocationManager Delegate
+
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location: CLLocationCoordinate2D = manager.location?.coordinate else {return}
+        print(location.latitude)
+        print(location.longitude)
+        mapView.animate(toLocation: location)
+        mapView.animate(toZoom: 1.0)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+}
+
+//MARK: - GMSAutocomplete Delegate
+
+
+extension ViewController: GMSAutocompleteViewControllerDelegate {
+  func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+    // Get the place name from 'GMSAutocompleteViewController'
+    // Then display the name in textField
+    locationField.text = place.name
+// Dismiss the GMSAutocompleteViewController when something is selected
+    dismiss(animated: true, completion: nil)
+  }
+func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+    // Handle the error
+    print("Error: ", error.localizedDescription)
+  }
+func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+    // Dismiss when the user canceled the action
+    dismiss(animated: true, completion: nil)
+  }
 }
 
